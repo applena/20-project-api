dfde2553e3f5aafe8c8760079dd1464a1ca3a379
Brent Woodward <Brent.Woodward.1@gmail.com>
2019-01-17 17:35:50 -0800

    merged api server with auth functionality, not hooked up

---
+++ src/auth/router.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const User = require('./users-model.js');
+const Role = require('./roles-model');
+const auth = require('./middleware.js');
+const oauth = require('./oauth/google.js');
+
+authRouter.post('/signup', (req, res, next) => {
+  let user = new User(req.body);
+  user.save()
+    .then( (user) => {
+      User.findOne({_id: user._id})
+        .then(user => {
+          req.token = user.generateToken();
+          req.user = user;
+          res.set('token', req.token);
+          res.cookie('auth', req.token);
+          res.send(req.token);
+        });
+    })
+    .catch(next);
+});
+
+authRouter.post('/signin', auth(), (req, res, next) => {
+  res.cookie('auth', req.token);
+  res.send(req.token);
+});
+
+authRouter.post('/newRole', (req, res, neext) => {
+  let role = new Role(req.body);
+  role.save();
+});
+
+authRouter.get('/oauth', (req,res,next) => {
+  oauth.authorize(req)
+    .then( token => {
+      res.status(200).send(token);
+    })
+    .catch(next);
+});
+
+authRouter.post('/key', auth, (req,res,next) => {
+  let key = req.user.generateKey();
+  res.status(200).send(key);
+});
+
+module.exports = authRouter;
